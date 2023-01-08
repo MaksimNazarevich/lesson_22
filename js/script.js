d6fd86a50cb5024bfb7ae8a951a4f8a88b1494c2
MaksimNazarevich <98347456+MaksimNazarevich@users.noreply.github.com>
2023-01-08 23:19:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,86 @@
+"use strict"
+//Домашнє завдання №1
+//Варіант №1
+
+console.log('Вчимо JS')
+
+//Варіант №2
+
+console.log('Вчимо')
+console.log(' JS')
+
+//Варіант №3
+
+// console.log('Вчимо') console.log(' JS') - Помилка
+
+//Варіант №3
+
+console.log('Вчимо');
+console.log(' JS');
+
+//Домашнє завдання № 2
+
+// let userColorEyes;
+// let user;
+// let userName;
+// userName = "Вася";
+// user = userName;
+
+// console.log(userName);
+
+
+//Варіант №1
+
+//let user = `Вася`  let age = 36 - помилка
+
+//Варіант №2
+
+let myAge = 36;
+
+//Варіант №3
+
+const BLOCK_SIZE = 14 + 50;
+
+//Варіант №4
+
+const 	BLOCK_HEIGHT = 100;
+
+//Варіант №5
+
+if(true){
+	var size = 15;
+}
+
+console.log(size);
+
+//Домашнє завдання № 3
+
+//Варіант №1
+
+let userAge = 36;
+let userInfo = 'Фрилансер ${userAge}'; //Помилка
+console.log(userInfo);
+
+//Варіант №2
+
+let userHeight = 145 / 0;
+//Повернеться значення NaN
+console.log(userHeight); //Тип даних Infinity
+
+//Варіант №3
+
+let userName;
+//Повернеться тип даних Null
+console.log(typeof userName); //Тип даних undefined
+
+//Варіант №4
+let userSize = "45" / "8";
+//Повернеться тип даних Number
+console.log(typeof userSize);
+
+
+
+
+
+
+
